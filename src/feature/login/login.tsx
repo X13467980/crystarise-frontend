62bf832a7cd02e64198b94ded74d78b214aa7920
handsomeK <ckyu388@gmail.com>
2025-08-12 22:45:56 +0900

    Login button

---
+++ src/feature/login/login.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const LoginButton = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsLoggedIn(true);
+    console.log("ログインしました");
+  };
+
+  const handleLogoutClick = () => {
+    setIsLoggedIn(false);
+    console.log("ログアウトしました");
+  };
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <button onClick={handleLogoutClick}>ログアウト</button>
+      ) : (
+        <button onClick={handleLoginClick}>ログイン</button>
+      )}
+    </div>
+  );
+};
+
+export default LoginButton;

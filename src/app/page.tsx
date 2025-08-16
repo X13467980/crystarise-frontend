883e3307b971bb8ab9338903229ed74785ada8d7
handsomeK <ckyu388@gmail.com>
2025-08-16 18:32:05 +0900

    仮アイコン追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 import LoginButton from "@/feature/login/loginbutton";
 import RegisterButton from "@/feature/register/registerbutton";
+import Image from "next/image"
 
 function Home() {
   return (
-    <div className ="border flex justify-center items-center h-screen">
-      <div className="border w-96 text-center">
+    <div className =" flex justify-center items-center h-screen">
+      <div className="border rounded-lg w-96 text-center">
       {/* ここにアイコンかな？ */}
-      <LoginButton />
-      <RegisterButton />
+        <div className="border-b-3 border-b-gray-500">
+          <Image
+            src="/2530064.png"
+            width={80}
+            height={80}
+            alt="fake icon"
+            className="mx-auto pb-3 border-b-4 border-b-white-500" 
+          />
+        </div>
+        <div className="mt-2">
+          <LoginButton />
+          <RegisterButton />
+        </div>
       </div>
     </div>
   );

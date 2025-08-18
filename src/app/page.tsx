a0c0224124c21e2a52cf77dce1ec80907482d9a3
handsomeK <ckyu388@gmail.com>
2025-08-18 22:51:17 +0900

    image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,27 @@ import Image from "next/image"
 
 function Home() {
   return (
-    <div className =" flex justify-center items-center h-screen">
-      <div className="border rounded-lg w-96 text-center">
-      {/* ここにアイコンかな？ */}
-        <div className="border-b-3 border-b-gray-500">
-          <Image
-            src="/2530064.png"
-            width={80}
-            height={80}
-            alt="fake icon"
-            className="mx-auto pb-3 border-b-4 border-b-white-500" 
-          />
-        </div>
-        <div className="mt-2">
-          <LoginButton />
-          <RegisterButton />
-        </div>
+  <div className="flex justify-center items-center min-h-screen">
+    <div>
+      <div className="">
+        <Image
+          src="/snowflake-icon.svg"
+          width={254}
+          height={229}
+        />
+        <Image
+          src="/name-icon.svg"
+          width={212}
+          height={65}
+          className="mx-auto"
+        />
+      </div>
+      <div className="text-center">
+        <LoginButton />
+        <RegisterButton />
       </div>
     </div>
+  </div>
   );
 }
 

fe248f9579c6fe6a74128594e894b26f9d2a2856
Youta Yano <0x59.xxx@gmail.com>
2025-08-19 00:15:06 +0900

    Fix: resolve conflict by merging feature/home visuals with develop-v1 layout; keep develop-v1 auth buttons; fix invalid Tailwind classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,31 @@ import Image from "next/image"
 
 function Home() {
   return (
-    <div className =" flex justify-center items-center h-screen">
-      <div className="border rounded-lg w-96 text-center">
-      {/* ここにアイコンかな？ */}
-        <div className="border-b-3 border-b-gray-500">
-          <Image
-            src="/2530064.png"
-            width={80}
-            height={80}
-            alt="fake icon"
-            className="mx-auto pb-3 border-b-4 border-b-white-500" 
-          />
-        </div>
-        <div className="mt-2">
-          <GoLoginButton />
-          <GoRegisterButton />
-        </div>
+
+  <div className="flex justify-center min-h-screen">
+    <div>
+      <div className="mt-37">
+        <Image
+          src="/snowflake-icon.svg"
+          width={254}
+          height={229}
+          alt="snowflake-icon"
+        />
+        <Image
+          src="/name-icon.svg"
+          width={212}
+          height={65}
+          className="mx-auto"
+          alt="Name-icon"
+        />
+      </div>
+      <div className="text-center mt-10" >
+        <GoLoginButton />
+        <GoRegisterButton />
+
       </div>
     </div>
+  </div>
   );
 }
 

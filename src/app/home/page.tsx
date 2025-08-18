47cf1ead38bd8eadef22e5a255b1a228edb4c942
Youta Yano <0x59.xxx@gmail.com>
2025-08-18 23:31:46 +0900

    Feat: add login button if user doesn't login

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,7 @@
 
 import Header from '@/feature/header/Header';
 import ProfileCard from '@/feature/profile/ProfileCard';
+import LoginButton from '@/feature/login/loginbutton';
 import StartButtons from '@/feature/startbuttons/StartButtons';
 import { useUserSummary } from '@/feature/hooks/useUserSummary';
 
@@ -47,6 +48,7 @@ export default function HomePage() {
         <Header />
         <main className="flex flex-col items-center justify-center px-6 py-16">
           <p className="opacity-90">ログインしてください</p>
+          <LoginButton />
           <StartButtons onSoloClick={handleSolo} onTeamClick={handleTeam} />
         </main>
       </div>

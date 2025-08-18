eb55f5fc6b8bb7eb3afa27d1be70884d19ef7d6a
Youta Yano <0x59.xxx@gmail.com>
2025-08-18 18:47:31 +0900

    Feat: make Header responsive and apply background color #144895

--- src/feature/header/Header.tsx
+++ src/feature/header/Header.tsx
@@ -2,9 +2,17 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="px-6 py-4 flex items-center justify-center ">
-      <div className="flex items-center">
-        <Image src="/logo.svg" alt="logo" width={150} height={40} />
+    <header
+      className="px-6 py-4 flex items-center justify-center text-white"
+      style={{ backgroundColor: "#144895" }}
+    >
+      <div className="relative w-40 md:w-60 lg:w-80 h-12 md:h-16 lg:h-20">
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          fill
+          className="object-contain"
+        />
       </div>
     </header>
   );

b11af77867a982a93305845b8630b428584c6b94
handsomeK <ckyu388@gmail.com>
2025-08-21 23:02:13 +0900

    image updated

--- src/feature/header/Header.tsx
+++ src/feature/header/Header.tsx
@@ -6,9 +6,9 @@ export default function Header() {
       className="px-6 py-4 flex items-center justify-center text-white"
       style={{ backgroundColor: "#144895" }}
     >
-      <div className="relative w-64 md:w-80 lg:w-[26rem] h-16 md:h-20 lg:h-24">
+      <div className="relative w-64 md:w-80 lg:w-[26rem] h-18 md:h-20 lg:h-24">
         <Image
-          src="/logo.svg"
+          src="/headerlogo.svg"
           alt="logo"
           fill
           className="object-contain"

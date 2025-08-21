93878585db1a52c75e86127302399173066a337b
handsomeK <ckyu388@gmail.com>
2025-08-21 23:59:21 +0900

    design change

--- src/feature/header/Header.tsx
+++ src/feature/header/Header.tsx
@@ -1,9 +1,12 @@
+"use client"
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+
 
 export default function Header() {
   return (
     <header
-      className="px-6 py-4 flex items-center justify-center text-white"
+      className="px-6 py-4 flex items-center justify-between  text-white"
       style={{ backgroundColor: "#144895" }}
     >
       <button className="relative w-18 md:w-20 lg:w-24 h-18 md:h-20 lg:h-24">
@@ -22,6 +25,7 @@ export default function Header() {
           className="object-contain"
         />
       </div>
+      <div></div>
     </header>
   );
 }

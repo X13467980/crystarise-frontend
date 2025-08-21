01f80490589bcb539a8fc30b981072c47a2021e6
handsomeK <ckyu388@gmail.com>
2025-08-21 23:35:02 +0900

    button added

--- src/feature/header/Header.tsx
+++ src/feature/header/Header.tsx
@@ -6,9 +6,17 @@ export default function Header() {
       className="px-6 py-4 flex items-center justify-center text-white"
       style={{ backgroundColor: "#144895" }}
     >
+      <button className="relative w-18 md:w-20 lg:w-24 h-18 md:h-20 lg:h-24">
+        <Image
+          src="homebutton.svg"
+          alt="home-button"
+          fill
+          className="object-contain"
+        />
+      </button>
       <div className="relative w-64 md:w-80 lg:w-[26rem] h-18 md:h-20 lg:h-24">
         <Image
-          src="/headerlogo.svg"
+          src="/logo.svg"
           alt="logo"
           fill
           className="object-contain"

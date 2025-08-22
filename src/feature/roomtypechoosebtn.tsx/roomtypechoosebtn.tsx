c667e696a8209737377c9ac950b2d4957d164857
Youta Yano <0x59.xxx@gmail.com>
2025-08-23 02:20:03 +0900

    Fix: fix rename

--- src/feature/roomtypechoosebtn.tsx/roomtypechoosebtn.tsx
+++ src/feature/roomtypechoosebtn.tsx/roomtypechoosebtn.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export default function RoomTypeChooseBtn({
     toOne ='/newroom',
-    toMany = '/TeamRoomChoice',
+    toMany = '/teamroomchoice',
     className = '',
 }: Props) {
     const router = useRouter()

324df11e34b94c524c8ca61003fd0f08d290baaa
Youta Yano <0x59.xxx@gmail.com>
2025-08-23 03:46:27 +0900

    Fix: fix rename

--- src/app/newroom/page.tsx
+++ src/app/newroom/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import Header from '@/feature/header/Header';
-import ProfileCard from '@/feature/profile/ProfileCard';
-import GoLoginButton from '@/feature/gologin/gologinbutton';
-import StartButtons from '@/feature/startbuttons/StartButtons';
+import Header from '@/feature/Header/Header';
+import ProfileCard from '@/feature/Profile/ProfileCard';
+import GoLoginButton from '@/feature/GoLogin/GoLoginButton';
+import StartButtons from '@/feature/StartButtons/StartButtons';
 import { useUserSummary } from '@/feature/hooks/useUserSummary';
-import LogoutButton from '@/feature/logout/LogoutButton';
+import LogoutButton from '@/feature/Logout/LogoutButton';
 import NewRoomInput from '@/feature/NewRoomInput/NewRoomInput';
 
 import React, { useState } from 'react';

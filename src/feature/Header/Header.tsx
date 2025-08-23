33039dc8554ecb8edfd01357d60e94c6362f9e3a
HSCK <ckyu388@gmail.com>
2025-08-24 03:03:07 +0900

    fix header

--- src/feature/Header/Header.tsx
+++ src/feature/Header/Header.tsx
@@ -1,2 +1,30 @@
-import Image from 'next/image'; export default function Header() 
-{ return ( <header className="px-6 py-4 flex items-start justify-between text-white" style={{ backgroundColor: "#144895" }} > <Image src="/Home.svg" width={52} height={70} alt="homebutton" /> <div className="relative w-64 md:w-80 lg:w-[26rem] h-16 md:h-20 lg:h-24"> <Image src="/logo.svg" alt="logo" fill className="object-contain" /> </div> <div className="w-[52px] h-[70]"> </div> </header> ); }
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export default function Header() {
+  const router = useRouter();
+
+  return (
+    <header
+      className="px-6 py-4 flex items-center justify-between text-white"
+      style={{ backgroundColor: "#144895" }}
+    >
+      <button
+        type="button"
+        onClick={() => router.push("/home")}
+        className="relative w-[52px] h-[70px]"
+        aria-label="home"
+      >
+        <Image src="/Home.svg" alt="homebutton" fill className="object-contain" />
+      </button>
+
+      <div className="relative w-64 md:w-80 lg:w-[26rem] h-16 md:h-20 lg:h-24">
+        <Image src="/logo.svg" alt="logo" fill className="object-contain" />
+      </div>
+
+      <div className="w-[52px] h-[70px]" />
+    </header>
+  );
+}

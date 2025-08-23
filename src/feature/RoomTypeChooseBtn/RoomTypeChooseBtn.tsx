34b97ed0ae877fd5dab2617ce29beb1670d3edd6
Youta Yano <0x59.xxx@gmail.com>
2025-08-23 14:03:58 +0900

    Fix: quick fix

--- src/feature/RoomTypeChooseBtn/RoomTypeChooseBtn.tsx
+++ src/feature/RoomTypeChooseBtn/RoomTypeChooseBtn.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export default function RoomTypeChooseBtn({
     toOne ='/newroom',
-    toMany = '/team',
+    toMany = '/teamroomchoice',
     className = '',
 }: Props) {
     const router = useRouter()

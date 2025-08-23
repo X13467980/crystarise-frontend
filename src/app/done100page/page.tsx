f7ff8fcfdd3833e07fe29b2e4fbc29e3df5f78bd
HSCK <ckyu388@gmail.com>
2025-08-24 04:00:52 +0900

    done100page

---
+++ src/app/done100page/page.tsx
@@ -0,0 +1,30 @@
+import Header from "@/feature/Header/Header"
+import Image from "next/image"
+
+export default function Done100Page() {
+    return (
+        <div className="">
+            <Header />
+            <div className="w-full min-h-screen flex justify-center mt-50">
+                <div className="w-72 h-105 flex flex-col justify-center items-center gap-5">
+                    <p className="font-mkpop font-">100% 達成！</p>
+                    <Image
+                    src="/fullsnowflake.svg"
+                    width={287}
+                    height={289}
+                    alt="snowflake"
+                    
+                    />
+                    <Image
+                    src="/information.svg"
+                    width={164}
+                    height={24}
+                    alt="information"
+                    />
+                    <button className="primary-btn mt-30">次へ</button>
+                </div>
+            </div>
+        </div>
+
+    )
+}

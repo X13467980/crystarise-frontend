14f973c7143a3f82ad99e7d32fa44d43c55dc499
HSCK <ckyu388@gmail.com>
2025-08-24 02:35:37 +0900

    add home button in header

--- src/feature/Header/Header.tsx
+++ src/feature/Header/Header.tsx
@@ -1,19 +1,2 @@
-import Image from 'next/image';
-
-export default function Header() {
-  return (
-    <header
-      className="px-6 py-4 flex items-center justify-center text-white"
-      style={{ backgroundColor: "#144895" }}
-    >
-      <div className="relative w-64 md:w-80 lg:w-[26rem] h-16 md:h-20 lg:h-24">
-        <Image
-          src="/logo.svg"
-          alt="logo"
-          fill
-          className="object-contain"
-        />
-      </div>
-    </header>
-  );
-}
+import Image from 'next/image'; export default function Header() 
+{ return ( <header className="px-6 py-4 flex items-start justify-between text-white" style={{ backgroundColor: "#144895" }} > <Image src="/Home.svg" width={52} height={70} alt="homebutton" /> <div className="relative w-64 md:w-80 lg:w-[26rem] h-16 md:h-20 lg:h-24"> <Image src="/logo.svg" alt="logo" fill className="object-contain" /> </div> <div className="w-[52px] h-[70]"> </div> </header> ); }

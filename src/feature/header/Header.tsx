f1d412c044f03109009b7844d5ef6eb27902e4af
Youta Yano <0x59.xxx@gmail.com>
2025-08-23 11:04:40 +0900

    Fix: fix rename

--- src/feature/header/Header.tsx
+++ src/feature/header/Header.tsx
@@ -1,28 +1,12 @@
-"use client";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Image from 'next/image';
 
 export default function Header() {
-  const router = useRouter();
-
   return (
     <header
-      className="relative px-6 py-4 flex items-center justify-between text-white"
+      className="px-6 py-4 flex items-center justify-center text-white"
       style={{ backgroundColor: "#144895" }}
     >
-      <button
-        onClick={() => router.push("/home")}
-        className="relative w-12 md:w-16 lg:w-20 h-12 md:h-16 lg:h-20"
-      >
-        <Image
-          src="/homebutton.svg"
-          alt="home-button"
-          fill
-          className="object-contain"
-        />
-      </button>
-
-      <div className="absolute left-1/2 transform -translate-x-1/2 w-48 md:w-64 lg:w-80 h-12 md:h-16 lg:h-20">
+      <div className="relative w-64 md:w-80 lg:w-[26rem] h-16 md:h-20 lg:h-24">
         <Image
           src="/logo.svg"
           alt="logo"
@@ -30,8 +14,6 @@ export default function Header() {
           className="object-contain"
         />
       </div>
-
-      <div className="w-12 md:w-16 lg:w-20 h-12 md:h-16 lg:h-20" />
     </header>
   );
 }
